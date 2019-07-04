be73fe76852159b6e577e5294f7d25e5282eebf0
jrangel <jrangel@ABG00901.abgam.es>
2019-07-04 11:26:46 +0200

    removed import sys, it was pointless

--- Mango/main.py
+++ Mango/main.py
@@ -4,10 +4,8 @@ Created on 26 jun. 2019
 @author: jrangel
 '''
 
-import sys
 import read_mango_humidity
 import read_rasp_temp
-sys.path.insert(0, 'C:\Users\jrangel\git\Mango_monitor\Mango')
 
 # Run until you get a ctrl^c
 try:
