1a7a488ab47c9cf9c8a31c7790458a214bf029b2
PC <PC@WhiteTower.lan>
2019-07-04 16:20:32 +0200

    Como miro la DB?

---
+++ Mango/read_serial.py
@@ -0,0 +1,18 @@
+'''
+Created on 4 jul. 2019
+
+@author: PC
+'''
+import serial
+
+def getSerialData ():
+
+    try: 
+        ser = serial.Serial('COM4', 9600) 
+        serial_line = ser.readline()
+        print(serial_line)
+        ser.close()
+    except: 
+        print ("Port coudnt be opened")
+        serial_line = ""
+    return serial_line
