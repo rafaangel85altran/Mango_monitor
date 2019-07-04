bbf0dd3f7ab47ae00918646e82d8daa2751356f0
jrangel <jrangel@ABG00901.abgam.es>
2019-07-04 09:12:06 +0200

    separated function that send data top the DB

--- Mango/main.py
+++ Mango/main.py
@@ -4,15 +4,16 @@ Created on 26 jun. 2019
 @author: jrangel
 '''
 
-import time
 import sys
-import datetime
+import read_mango_humidity
+sys.path.insert(0, 'C:\Users\jrangel\git\Mango_monitor\Mango')
+
 from influxdb import InfluxDBClient
 
 # Configure InfluxDB connection variables
-host = "192.168.1.213"                          # My Ubuntu NUC
-port = 8086                                     #default port
-user = "admin"                                  # the user/password created for the pi, with write access
+host = "192.168.1.213"                          # Raspberri pi Roja Ip en ZGZ
+port = 8086                                     #default port for InfluxDB
+user = "admin"                                  # the user/password created for influxDB
 password = "emperador" 
 dbname = "telegraf"                             #the database we created earlier
 interval = 5                                    #Sample period in seconds
@@ -20,45 +21,11 @@ interval = 5                                    #Sample period in seconds
 # Create the InfluxDB client object
 client = InfluxDBClient(host, port, user, password, dbname)
 
-# Enter the sensor details
-sensor = 0
-sensor_gpio = 0
-
-# think of measurement as a SQL table, it's not...but...
-measurement = "test saw"
-# location will be used as a grouping tag later
-location = "ZGZ Boggiero"
-
-data_test = 0
-
 # Run until you get a ctrl^c
 try:
     while True:
-        # Read the sensor using the configured driver and gpio
-        data_test = 2 + data_test 
-        iso = time.ctime()
-        # Print for debugging, uncomment the below line
-        # print("[%s] Temp: %s, Humidity: %s" % (iso, temperature, humidity)) 
-        # Create the JSON data structure
-        data = [
-        {
-          "measurement": measurement,
-              "tags": {
-                  "location": location,
-              },
-              "time": iso,
-              "fields": {
-                  "temperature" : data_test
-              }
-          }
-        ]
-        # Send the JSON data to InfluxDB
-        client.write_points(data)
-        # Wait until it's time to query again...
-        time.sleep(interval)
-        if (data_test > 25):
-            data_test = 0
-        print("Sending", data_test,"...")    
-            
+        read_mango_humidity.getMangoHumidity()
 except KeyboardInterrupt:
     pass
+except:
+    print("Error during execution, finishing")
---
+++ Mango/read_mango_humidity.py
@@ -0,0 +1,48 @@
+'''
+Created on 4 jul. 2019
+
+@author: jrangel
+'''
+
+import time
+from main import interval
+from main import client
+
+# think of measurement as a SQL table, it's not...but...
+measurement = "test saw"
+# location will be used as a grouping tag later
+location = "ZGZ Boggiero"
+room = "Terraza"
+place = "Maceta Mango"
+
+data_test = 0
+
+
+def getMangoHumidity():
+    # Data to store
+    data_test = 2 + data_test 
+    iso = time.ctime()
+    # Print for debugging, uncomment the below line
+    # print("[%s] Temp: %s, Humidity: %s" % (iso, temperature, humidity)) 
+    # Create the JSON data structure
+    data = [
+    {
+      "measurement": measurement,
+          "tags": {
+              "location": location,
+              "room"    : room,
+              "place"   : place
+          },
+          "time": iso,
+          "fields": {
+              "temperature" : data_test
+          }
+      }
+    ]
+    # Send the JSON data to InfluxDB
+    client.write_points(data)
+    # Wait until it's time to query again...
+    time.sleep(interval)
+    if (data_test > 25):
+        data_test = 0
+    print("Sending", data_test,"...")    
