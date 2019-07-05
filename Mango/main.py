e2bd9432c3d3ac1a3b4e86ecc58f4bddde2daa59
rafaangel85altran <34768304+rafaangel85altran@users.noreply.github.com>
2019-07-05 14:56:04 +0200

    espeak usage when main.py is executed though boot

--- Mango/main.py
+++ Mango/main.py
@@ -4,8 +4,12 @@ Created on 26 jun. 2019
 @author: jrangel
 '''
 
+from subprocess import call
+
 import read_mango_humidity
 #import read_rasp_temp
+
+call([‘espeak “Staring main.py that uploads Humidity and Rasp Temp” 2>/dev/null’], shell=True)       #sudo apt-get install espeak
 
 # Run until you get a ctrl^c
 try:
