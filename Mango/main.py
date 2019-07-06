f80ffb71c4b335bbf47b78737ca0b4a194a3d468
PC <PC@WhiteTower.lan>
2019-07-06 02:42:15 +0200

    Added 60 seconds of sleep so the rasp can run the InfluxDB correctly

--- Mango/main.py
+++ Mango/main.py
@@ -4,8 +4,14 @@ Created on 26 jun. 2019
 @author: jrangel
 '''
 
+import time
 import read_mango_humidity
 import read_rasp_temp
+
+
+#Wait at least 60seconds so the rasp can connect to the Wifi
+
+time.sleep(60)
 
 # Run until you get a ctrl^c
 try:
